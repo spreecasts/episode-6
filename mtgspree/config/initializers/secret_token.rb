d80f4ff2254f5599d32b3df8868ff3e0819f31ed
Peter Berkenbosch <peter@pero-ict.nl>
2013-03-11 14:21:28 +0100

    add and installed spree

---
+++ mtgspree/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtgspree::Application.config.secret_token = '1d8f192d7e2cafb940ff626001982e349912993387bedef730a72e19527cb2f016fb93c09aa0401a291138a87f02eee4713b118d672af531bc60f695316db6d7'
